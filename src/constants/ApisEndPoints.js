93ce45547608e0af994542bf111fe016201f8e9b
Muhammad Afaq Riaz <m.afaqriaz07@gmail.com>
2021-05-03 21:59:58 +0500

    Api endpoint

--- src/constants/ApisEndPoints.js
+++ src/constants/ApisEndPoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "http://saloonfinder.imtekh.com/api/";
 
 export const LOGIN_URL = BASE_URL + "user/login";
 export const REGISTER_URL = BASE_URL + "user/register";
